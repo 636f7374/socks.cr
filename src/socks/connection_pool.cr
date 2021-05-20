fcb5ba2bec10797ed40bdb2e4d4064b77cf9354a
_ <53088156+636f7374@users.noreply.github.com>
2021-05-21 03:41:09 +0800

    Improve ConnectionPool.

--- src/socks/connection_pool.cr
+++ src/socks/connection_pool.cr
@@ -1,103 +1,98 @@
 class SOCKS::ConnectionPool
   getter clearInterval : Time::Span
   getter capacity : Int32
-  getter entries : Hash(UInt64, Entry)
+  getter entries : Set(Entry)
   getter latestCleanedUp : Time
   getter mutex : Mutex
 
   def initialize(@clearInterval : Time::Span = 10_i32.seconds, @capacity : Int32 = 5_i32)
-    @entries = Hash(UInt64, Entry).new
+    @entries = Set(Entry).new
     @latestCleanedUp = Time.local
     @mutex = Mutex.new :unchecked
   end
 
   def clear
     @mutex.synchronize do
-      entries.each do |object_id, entry|
+      entries.each do |entry|
+        entries.delete entry
         entry.transfer.cleanup side: Transfer::Side::Destination, free_tls: true, reset: true
-        entries.delete object_id
       end
-    end
-  end
-
-  def size : Int32
-    @mutex.synchronize { entries.size }
-  end
-
-  def unshift(value : Transfer)
-    inactive_entry_cleanup
-    unshift object_id: value.object_id, value: value
-  end
-
-  private def unshift(object_id : UInt64, value : Transfer)
-    @mutex.synchronize do
-      if entry = entries[object_id]?
-        entry.transfer.cleanup side: Transfer::Side::Destination, free_tls: true, reset: true
-        entries.delete object_id
-      end
-
-      entries[object_id] = Entry.new transfer: value
     end
   end
 
   private def need_cleared?
-    interval = Time.local - (@mutex.synchronize { latestCleanedUp })
+    interval = Time.local - latestCleanedUp.dup
     interval > clearInterval
   end
 
-  private def refresh_latest_cleaned_up
-    @mutex.synchronize { @latestCleanedUp = Time.local }
+  def inactive_entry_cleanup_mutex
+    @mutex.synchronize { inactive_entry_cleanup }
   end
 
-  def inactive_entry_cleanup
-    @mutex.synchronize do
-      while capacity <= entries.size
-        object_id, entry = entries.first
-        entry.transfer.cleanup side: Transfer::Side::Destination, free_tls: true, reset: true
-        entries.delete object_id
-      end
+  private def inactive_entry_cleanup
+    while capacity <= entries.size
+      entry = entries.first
+
+      entries.delete entry
+      entry.transfer.cleanup side: Transfer::Side::Destination, free_tls: true, reset: true
     end
 
     return unless need_cleared?
 
-    @mutex.synchronize do
-      entries.each do |object_id, entry|
-        next unless clearInterval <= (Time.local - entry.createdAt)
-        entry.transfer.cleanup side: Transfer::Side::Destination, free_tls: true, reset: true
-        entries.delete object_id
-      end
+    entries.each do |entry|
+      next unless clearInterval <= (Time.local - entry.created_at)
+
+      entries.delete entry
+      entry.transfer.cleanup side: Transfer::Side::Destination, free_tls: true, reset: true
     end
 
     refresh_latest_cleaned_up
   end
 
-  def get? : Transfer?
-    inactive_entry_cleanup
+  private def refresh_latest_cleaned_up
+    @latestCleanedUp = Time.local
+  end
 
+  def size : Int32
+    @mutex.synchronize { entries.size.dup }
+  end
+
+  def unshift(value : Transfer)
     @mutex.synchronize do
+      inactive_entry_cleanup
+      entries << Entry.new transfer: value
+    end
+  end
+
+  def get? : Transfer?
+    @mutex.synchronize do
+      inactive_entry_cleanup
+
       loop do
         break unless entries_first = entries.first?
+        entries.delete entries_first
 
-        object_id, entry = entries_first
-        entries.delete object_id
-
-        if clearInterval <= (Time.local - entry.createdAt)
-          entry.transfer.cleanup side: Transfer::Side::Destination, free_tls: true, reset: true
+        if clearInterval <= (Time.local - entries_first.created_at)
+          entries_first.transfer.cleanup side: Transfer::Side::Destination, free_tls: true, reset: true
 
           next
         end
 
-        break entry.transfer
+        break entries_first.transfer
       end
     end
   end
 
   struct Entry
-    property transfer : Transfer
-    property createdAt : Time
+    getter transfer : Transfer
+    getter createdAt : Time
 
     def initialize(@transfer : Transfer)
       @createdAt = Time.local
+    end
+
+    def created_at : Time
+      @createdAt
     end
   end
 end
